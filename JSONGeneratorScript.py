b52053821078a679e06d00b40a39bf8b8b0303e7
MatteoAlberghini <mado.alberghini.main@gmail.com>
2019-01-29 18:26:04 +0100

    commit 4: fixed bugs still no nested json

--- JSONGeneratorScript.py
+++ JSONGeneratorScript.py
@@ -1,13 +1,12 @@
 import json
 import os
-import pandas
 
 # Ask filename
 file_name = input("Enter JSON file name: ")
 
 # Gets desktop path
 desktop = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop')
-completeName = os.path.join(desktop, file_name)
+complete_name = os.path.join(desktop, file_name)
 
 # Function to detect if string is a number
 
@@ -22,24 +21,20 @@ def is_number(s):
 
 # Ask number of input and checks if is an actual number
 number = 0
+array_name_input = []
+print("Enter name of the input or write EXIT to quit the input string \n")
 while True:
-    number_of_input = input("Enter number of input: ")
-    if number_of_input.isnumeric():
-        number = int(number_of_input)
+    temp = number + 1
+    inp = input("Insert string number(%d): " % temp)
+    if inp == "EXIT":
         break
     else:
-        print("Input needs to be an integer")
+        number += 1
+        array_name_input.append(inp)
 
-# Ask for all name in the dictionary
-array_name_input = []
-for i in range(0, number):
-    temp = i+1
-    array_name_input.append(input("Insert string number(%d): " % temp))
 
 # For each of the string he puts in, we ask the data
-
 data_array = []
-
 for i in range(0, len(array_name_input)):
     data = {}
     for string in array_name_input:
@@ -53,6 +48,6 @@ for i in range(0, len(array_name_input)):
 
 
 # Write on desktop with filename as the name of the file
-with open(completeName + ".json", "w") as f:
+with open(complete_name + ".json", "w") as f:
     json.dump(data_array, f)
 
